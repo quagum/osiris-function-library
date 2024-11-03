d943dc6a3ad56343cde9e37c78b0530029eeec1d
jpai-15 <pai.jhanvipai@gmail.com>
2024-11-03 00:21:59 -0400

    test cases for API 2 and 3

--- core-management/tests/test_util.py
+++ core-management/tests/test_util.py
@@ -2,6 +2,23 @@ from unittest.mock import MagicMock
 from src import util
 
 
+#update_function tests
+def test_fail_update_function():
+    util.updateFunctionInLibrary = MagicMock(return_value=False)
+    assert util.updateFunctionInLibrary("function_name", "code", "version") == False
+
+def test_success_update_function():
+    util.updateFunctionInLibrary = MagicMock(return_value=True)
+    assert util.updateFunctionInLibrary("function_name", "code", "version") == True
+
+#remove_function tests
+def test_fail_remove_function():
+    util.removeFunctionFromLibrary = MagicMock(return_value=False)
+    assert util.removeFunctionFromLibrary("function_name") == False
+def test_success_remove_function():
+    util.removeFunctionFromLibrary = MagicMock(return_value=True)
+    assert util.removeFunctionFromLibrary("function_name") == True
+
 #archive_function tests
 def test_fail_archive_function():
     util.archiveFunction = MagicMock(return_value=False)
