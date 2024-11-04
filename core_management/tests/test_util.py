052b75f85e5c7a3da7b2b775fd0941442535e1bc
jpai-15 <pai.jhanvipai@gmail.com>
2024-11-04 18:24:26 -0500

    removeFunction tests

--- core_management/tests/test_util.py
+++ core_management/tests/test_util.py
@@ -19,6 +19,14 @@ def test_success_update_function():
     util.updateFunctionInLibrary = MagicMock(return_value=True)
     assert util.updateFunctionInLibrary("function_name", "code", "version") == True
 
+#remove_function tests
+def test_fail_remove_function():
+    util.removeFunctionFromLibrary = MagicMock(return_value=False)
+    assert util.removeFunctionFromLibrary("function_name") == False
+def test_success_remove_function():
+    util.removeFunctionFromLibrary = MagicMock(return_value=True)
+    assert util.removeFunctionFromLibrary("function_name") == True
+
 #archive_function tests
 def test_fail_archive_function():
     util.archiveFunction = MagicMock(return_value=False)
